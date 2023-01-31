279d09fb374756066ef23efdd6d229bbb3451063
Mateusz Czyzewski <mati.cz@wp.pl>
2023-01-31 10:30:40 +0100

    Add TS types

---
+++ command.d.ts
@@ -0,0 +1,11 @@
+import { Options } from './index.d';
+
+declare function addMatchImageSnapshotCommand(
+  nameOrOptions?: string | Options
+): void;
+declare function addMatchImageSnapshotCommand(
+  name: string,
+  options: Options
+): void;
+
+export { addMatchImageSnapshotCommand };
